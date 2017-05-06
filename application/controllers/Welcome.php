d29eb5e05229502f59292d47386ccc004f74c3a1
LukasTelvak <lukastelvakn.e.x.u.s@gmail.com>
2017-05-06 10:39:31 +0200

    navbar a ikona

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,6 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
         $this->load->view('template/header');
+        $this->load->view('template/navigation');
 		$this->load->view('about');
         $this->load->view('template/footer');
 	}
